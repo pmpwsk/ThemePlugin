c1d16e383b395dc6f6e6cd30ce65855e4763faab
Flo <flo@uwap.org>
2024-01-14 01:41:36 +0100

    Added an API handler.

---
+++ API.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace uwap.WebFramework.Plugins;
+
+public partial class ThemePlugin : Plugin
+{
+    public override async Task Handle(ApiRequest req, string path, string pathPrefix)
+    {
+        switch(path)
+        {
+            default:
+                req.Status = 404;
+                break;
+        }
+    }
+}
