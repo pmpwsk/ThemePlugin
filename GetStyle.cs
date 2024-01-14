3c59bad4c4c7956567dc45e0653c3bbdb49fb7f4
Flo <flo@uwap.org>
2024-01-14 01:42:38 +0100

    Added GetStyle().

---
+++ GetStyle.cs
@@ -0,0 +1,21 @@
+﻿using uwap.WebFramework.Elements;
+
+namespace uwap.WebFramework.Plugins;
+
+public partial class ThemePlugin : Plugin
+{
+    private string[] Themes = ["code", "dark", "light", "white"];
+
+    public Style GetStyle(IRequest request, out string fontUrl, string pathPrefix)
+        => GetStyle((request.LoggedIn && request.User.Settings.TryGetValue("Theme", out string? colors) && Themes.Contains(colors)) ? colors : "dark", out fontUrl, pathPrefix);
+
+    private Style GetStyle(string colors, out string fontUrl, string pathPrefix)
+    {
+        if (Themes.Contains(colors))
+        {
+            fontUrl = $"{pathPrefix}/fonts/ubuntu{(colors == "code" ? "-mono" : "")}.woff2";
+            return new Style($"/api{pathPrefix}/build.css?colors={colors}&t={new[] { "/base.css", $"/{colors}.css" }.Max(x => long.Parse(GetFileVersion(x) ?? "0"))}");
+        }
+        else throw new ArgumentOutOfRangeException(nameof(colors));
+    }
+}
